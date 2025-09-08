e4359721ecb7c8b3c143699235d8763d155f6da1
Bryan T. Richardson <bryan@activeshadow.com>
2025-09-08 11:23:08 -0400

    fix: move from GitPod to Codespaces for cloud deployment

---
+++ .devcontainer/setup.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+apt update && apt install -y git-lfs unzip
+
+git lfs pull
+
+curl -L -o /tmp/opensearch.zip "https://grafana.com/api/plugins/grafana-opensearch-datasource/versions/2.13.0/download?os=linux&arch=amd64"
+unzip -d configs/grafana/plugins /tmp/opensearch.zip
+
+docker compose pull wireshark main-ctlr opensearch grafana
+docker compose up -d
